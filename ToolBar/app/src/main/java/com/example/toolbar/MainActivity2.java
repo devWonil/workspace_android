9300e843f95c7317f67d7997ca0bb9855af2130d
devWonil <102580743+devWonil@users.noreply.github.com>
2022-05-24 18:25:25 +0900

    toolbar

---
+++ ToolBar/app/src/main/java/com/example/toolbar/MainActivity2.java
@@ -0,0 +1,79 @@
+package com.example.toolbar;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
+
+    // 값을 돌려 받을 때 사용하는 코드
+    ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                Log.d("TAG", "comeback 돌아왔다");
+                if(result.getResultCode() == Activity.RESULT_OK){
+                    //정상 동작
+                    Intent data = result.getData();
+                    int returnValue = data.getIntExtra("result", 0);
+                    textView.setText(returnValue + "");
+                }else {
+                    //실패
+                }
+            }
+    );
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+       Toolbar toolbar = findViewById(R.id.toolBar);
+       //toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
+        textView = findViewById(R.id.tempTextView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuItem1:
+                Intent intent = new Intent(this, SubActivity.class);
+                intent.putExtra("value1", 10);
+                // 값을 돌려 받지 않을 때
+                //startActivity(intent);
+
+                // 값을 돌려 받아야 될 때
+                startActivityResult.launch(intent);
+                break;
+            case R.id.menuItem2:
+                Log.d("TAG", "2번 클릭");
+                break;
+            case R.id.menuItem3:
+                Log.d("TAG", "3번 클릭");
+                break;
+        }
+
+        return true;
+    }
+}
---
+++ ToolBar/app/src/main/java/com/example/toolbar/SubActivity.java
@@ -0,0 +1,34 @@
+package com.example.toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+        Button button = findViewById(R.id.btnFinish);
+        int value1 = getIntent().getIntExtra("value1", 0);
+        Log.d("TAG", "value1 : " + value1);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                // 값을 mainActivity에 보내기
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("result", value1 + 10);
+                // 반드시 기억하기
+                setResult(Activity.RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+    }
+}
