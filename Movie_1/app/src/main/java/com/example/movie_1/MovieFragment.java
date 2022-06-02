06297cb68fc57309f07d6e18e928bc95cf0751bb
devWonil <102580743+devWonil@users.noreply.github.com>
2022-06-02 17:48:51 +0900

    yts.mx 영화정보

---
+++ Movie_1/app/src/main/java/com/example/movie_1/MovieFragment.java
@@ -0,0 +1,100 @@
+package com.example.movie_1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.example.movie_1.adapter.MovieAdapter;
+import com.example.movie_1.databinding.FragmentMovieBinding;
+import com.example.movie_1.models.Movie;
+import com.example.movie_1.models.YtsData;
+import com.example.movie_1.repository.MovieService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class MovieFragment extends Fragment {
+
+    // 안드로이드에서 만들어 준 클래스
+    private FragmentMovieBinding binding;
+    private static final String TAG = MovieFragment.class.getName();
+    // 여기서 통신 요청을 할 예정
+    private MovieService movieService;
+
+    private MovieAdapter movieAdapter;
+    private List<Movie> list = new ArrayList<>();
+    public MovieFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static MovieFragment newInstance() {
+        MovieFragment fragment = new MovieFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        movieService  = MovieService.retrofit.create(MovieService.class);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // findViewById 대체 (뷰 바인딩 활용)
+        // inflater.inflate(R.layout.fragment_movie, container, false);
+        binding = FragmentMovieBinding.inflate(inflater, container, false);
+        // RecyclerView 만들어주기
+
+        // 아직 없음 (안드로이드 입체적으로 생각)
+        setupRecyclerView(list);
+        requestMoviesData(1);
+        return binding.getRoot();
+    }
+
+    private void requestMoviesData(int requestPage) {
+
+        movieService.repoContributors("rating", 10,  requestPage)
+                .enqueue(new Callback<YtsData>() {
+                    @Override
+                    public void onResponse(Call<YtsData> call, Response<YtsData> response) {
+                        if(response.isSuccessful()) {
+                            // 통신을 통해서 데이터를 넘겨받았으면 adapter에 데이터를 전닿해서 화면을 갱신처리
+                            List<Movie> list = response.body().getData().getMovies();
+                            movieAdapter.addItemList(list);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<YtsData> call, Throwable t) {
+                        Log.d(TAG, t.getMessage());
+                    }
+                });
+    }
+    // 통신해서 JSON 받은 다음에 매개변수로 데이터를 넘길 예정
+    private void setupRecyclerView(List<Movie> movieList) {
+        // 1. 어댑터
+        movieAdapter = new MovieAdapter();
+        movieAdapter.addItemList(movieList);
+        // 2. 매니저 필요
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        // 3. xml 파일에 선언한 recyclerView에 세팅
+        binding.movieRecyclerView.setAdapter(movieAdapter);
+        binding.movieRecyclerView.setLayoutManager(manager);
+        binding.movieRecyclerView.hasFixedSize();
+        // 이벤트 리스너
+    }
+}
---
+++ Movie_1/app/src/main/java/com/example/movie_1/adapter/MovieAdapter.java
@@ -0,0 +1,106 @@
+package com.example.movie_1.adapter;
+
+// 내부 클래스 먼저 만들기
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.FitCenter;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.example.movie_1.R;
+import com.example.movie_1.models.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder> {
+
+    private List<Movie> list = new ArrayList<>();
+
+    // 통신 배우기전 생성자에 데이터를 전달 받아서 화면을 구성
+    // 통신이기 때문에 화면을 그리는 시점보다 더 늦게 데이터가 도달 할 수 있다.
+
+    public void addItemList(List<Movie> list) {
+        this.list = list;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.item_movie_card, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        Movie movie = list.get(position);
+//        // 1. 여기서 바로 세팅하기
+//        holder.titleTv.setText(movie.getTitle());
+//        holder.ratingTv.setText(String.valueOf(movie.getRating()));
+//        // 9.312 () --> Math.floor()
+//        holder.ratingBar.setRating((float) movie.getRating());
+//
+//        Glide.with(holder.posterIv.getContext())
+//                .load(movie.getMediumCoverImage())
+//                .placeholder(R.drawable.round_image)
+//                .transform(new FitCenter(), new RoundedCorners(20))
+//                .into(holder.posterIv);
+
+        // 2. view holder 안에서 세팅
+        // 메서드 (movie)
+        holder.setItem(movie);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    // inner static class
+    // view binding 뷰결합 안됨(리사이클러뷰), 직접해야함 -> 단방향
+    // data binding 뷰결합 -> 양방향
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        // itemView 에 선언한 데이터 타입들 선언하고 생성 시점에 R.id.xxx 연결해주면 된다.
+        private View itemView;
+        private ImageView posterIv;
+        private TextView titleTv;
+        private TextView ratingTv;
+        private RatingBar ratingBar;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.itemView = itemView;
+            posterIv = itemView.findViewById(R.id.posterIv);
+            titleTv = itemView.findViewById(R.id.titleTv);
+            ratingTv = itemView.findViewById(R.id.ratingTv);
+            ratingBar = itemView.findViewById(R.id.ratingBar);
+        }
+
+        public void setItem(Movie movie) {
+            titleTv.setText(movie.getTitle());
+            ratingTv.setText(String.valueOf(movie.getRating()));
+            // 9.312 () --> Math.floor()
+            ratingBar.setRating((float) movie.getRating());
+
+            Glide.with(posterIv.getContext())
+                    .load(movie.getMediumCoverImage())
+                    .placeholder(R.drawable.round_image)
+                    .transform(new FitCenter(), new RoundedCorners(20))
+                    .into(posterIv);
+        }
+    }
+}
